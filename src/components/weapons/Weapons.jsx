b560cbf8c49054db577f0d515bc9535ec1c7308a
LUIS GERARDO MEDINA MORENO <luis.medina2@ulatina.net>
2023-12-29 23:50:00 -0600

    weapon's components added

--- src/components/weapons/Weapons.jsx
+++ src/components/weapons/Weapons.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './Weapons.css'
+import WeaponsContent from './WeaponsContent'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Weapons = () => {
+
+  const [weapons, setWeapons] = useState([])
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://valorant-api.com/v1/weapons');
+        const data = await response.json();
+        setWeapons(data.data)
+      }catch(error){
+        console.log("no tan facilito:(", error)
+      }
+    };
+    fetchData();
+  }, [])
+
+  console.log(weapons)
+
+  
   return (
-    <div>Weapons</div>
+    <div className='weapons-sc'>
+      <div className='weapons-info'>
+        <h1>Choose your <span>weapon</span></h1>
+        <p>Creativity is your greastest weapon</p>
+      </div>
+
+      <div className='weapons-sc-content'>
+      <WeaponsContent weapons={weapons} />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/weapons/WeaponsContent.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Slider from 'react-slick';
+
+const WeaponsContent = ({weapons}) => {
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,  
+        slidesToScroll: 1
+      };
+
+  return (
+    <>
+
+    <Slider {...settings}>
+      {weapons.map((weapon) => (
+        <div key={weapon.uuid} className="slider-weapons">
+            <div className='weapon-img-content'>
+                <img src={weapon.displayIcon} alt={weapon.displayName} />
+            </div>
+            <div className='weapon-info'>
+                <div className='bg-weapons-sc'></div>
+                <div className='weapon-info-content'>
+                    <h1>{weapon.displayName}</h1>
+                    <h2>{weapon.shopData?.category}</h2>
+                </div>
+            </div>
+        </div>
+      ))}
+    </Slider>
+
+    </>
+  )
+}
+
+export default WeaponsContent
